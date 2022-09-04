69090ce10152648f27e924df79e9e72afa84b425
JazminRondon <Jazmin_Rondon@outlook.com>
2022-09-04 19:23:40 -0400

    second part

--- src/main/java/BeerSong.java
+++ src/main/java/BeerSong.java
@@ -1,4 +1,4 @@
-lspublic class BeerSong {
+public class BeerSong {
     public static void main(String[] args) {
 
         // variables
---
+++ src/main/java/PhraseOMatic.java
@@ -0,0 +1,25 @@
+
+public class PhraseOMatic {
+    public static void main(String[] args) {
+        String[] wordListOne = {"no more HW", "completed", "second one", "air head"};
+        String[] wordListTwo = {"focus", "finish up", "count down", "grit", "motivation"};
+        String[] wordListThree = {"likeable", "studious", "helpful", "study group", "extra credit", "another one"};
+
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+
+        //generate three random numbers
+        int rand1 = (int) (Math.random() * oneLength);
+        int rand2 = (int) (Math.random() * twoLength);
+        int rand3 = (int) (Math.random() * threeLength);
+
+        // now build a phrase
+        String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + wordListThree[rand3];
+
+        //print phrase
+
+        System.out.println("We are so happy: " + phrase);
+    }
+}
+
