561e7a29b0d64326d18e162ded2660139b85ac6f
JazminRondon <Jazmin_Rondon@outlook.com>
2022-09-04 19:53:00 -0400

    hw 3 completed

---
+++ src/main/java/homework3/Pokemon.java
@@ -0,0 +1,25 @@
+package homework3;
+
+public class Pokemon {
+
+    String name;
+    int level;
+
+    Pokemon(){
+        level = 1;
+    }
+
+    Pokemon(String name, int level){
+        this.name = name;
+        this.level = level;
+
+    }
+
+    public void attack(){
+        System.out.println(name + " Attack!");
+    }
+
+
+
+
+}
